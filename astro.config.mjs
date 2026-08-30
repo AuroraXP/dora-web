cde27348b3440b048bdf5a7fd469f8f9fb0e35b2
AuroraXP <quackium@proton.me>
2026-08-30 14:10:41 +0200

    config  changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import solidJs from "@astrojs/solid-js";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig, passthroughImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
-	image: { service: passthroughImageService() },
 	integrations: [solidJs()],
 	vite: { plugins: [tailwindcss()] },
 });
