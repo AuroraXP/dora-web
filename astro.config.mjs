1b99307f32045f825917d255c3b7368ee3fc6b59
Patrick Konrad Piel <patrick@prismaticweb.io>
2026-08-30 14:25:59 +0200

    add cloudflare adapter (proper deloy handling)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 // @ts-check
+
+import cloudflare from "@astrojs/cloudflare";
 import solidJs from "@astrojs/solid-js";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
@@ -6,4 +8,5 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
 	integrations: [solidJs()],
 	vite: { plugins: [tailwindcss()] },
+	adapter: cloudflare({ imageService: "passthrough" }),
 });
